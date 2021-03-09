d16d4d7c4f221eaab327f2b035157f84fba91767
Tom Goffrey <t.goffrey.1@warwick.ac.uk>
2021-03-09 20:30:55 +0000

    Add ability to specify initial velocity in Ford 1991 set-up

--- python/ford1991.py
+++ python/ford1991.py
@@ -10,8 +10,8 @@ def rhs(t, x, omega, mass, tau):
     power = tau * mass * (accx**2 + accy**2)
     return [x[2], x[3], accx, accy, power]
 
-# Analytic solution. Assumes motion initially vertical
-def analytic_solution(t, omega, tau):
+# Analytic solution. Assumes motion initially vertical at t=0, magnitude vel0
+def analytic_solution(t, omega, tau, vel0=1.0):
     mu = tau * omega**2
 
     vx_soln = (np.exp(-mu * t) * np.sin(omega*t)) / (1.0 + tau**2 * omega**2)
@@ -23,18 +23,24 @@ def analytic_solution(t, omega, tau):
     y_soln = np.exp(-mu * t) * (omega * np.sin(omega * t)
                                    - mu * np.cos(omega * t)) / ((omega**2 + mu**2)
                                                                 * (1 + tau**2 * omega**2))
+    # Scale results by initial velocity
+    vx_soln *= vel0
+    vy_soln *= vel0
+    x_soln *= vel0
+    y_soln *= vel0
+
     return x_soln, y_soln, vx_soln, vy_soln
 
 # Solve Ford & O'Connell 1991 for n rotations
 # Additionally solve for total radiated power as function of time
-def solve(n_rotations, omega, mass, tau):
+def solve(n_rotations, omega, mass, tau, vel0=1.0):
     # Maximum timestep. Could probably be smaller
     max_step = 1e-3 / np.abs(omega)
     # Final time
     t_end = n_rotations * 2.0 * np.pi / np.abs(omega)
 
     # Set initial conditions
-    x_init, y_init, vx_init, vy_init = analytic_solution(0, omega, tau)
+    x_init, y_init, vx_init, vy_init = analytic_solution(0, omega, tau, vel0=vel0)
     # Note that for tau /= 0, both x_init and y_init and non-zero
     # Final value is total radiated power
     ic = [x_init, y_init, vx_init, vy_init, 0.0]
