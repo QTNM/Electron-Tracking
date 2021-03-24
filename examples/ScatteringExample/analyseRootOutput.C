caba6e596020d53bb2e2f1e32365dfde118d9217
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2021-03-24 13:35:50 +0000

    [analysis] add simple counting function for analysis.

--- examples/ScatteringExample/analyseRootOutput.C
+++ examples/ScatteringExample/analyseRootOutput.C
@@ -3,7 +3,45 @@
 #include <TTreeReaderValue.h>
 
 #include <iostream>
-#include <vector>
+
+//  use output TTree for short summary
+void shortsummary(TString fname) {
+  if (fname.IsNull()) fname = "qtnm.root";
+  TFile *fin = new TFile(fname.Data(), "READ");
+  TTreeReader nt1("Score", fin);
+  TTreeReaderValue<int>    evid1(nt1, "EventID");
+  TTreeReaderValue<double> kine(nt1, "KinE");
+  TTreeReaderValue<double> time(nt1, "Time");
+  TTreeReaderValue<int>    tid(nt1, "HitID");
+  TTreeReaderValue<int>    pid(nt1, "ParentID");
+   
+  TTreeReader nt2("Watch", fin);
+  TTreeReaderValue<int>    evid2(nt2, "EventID");
+  TTreeReaderValue<double> btime(nt2, "ExitTime");
+  TTreeReaderValue<int>    btid(nt2, "ExitID");
+      
+  // event loop
+  int counterh = 0;
+  int counterw = 0;
+  int prevev   = -1;
+  while (nt1.Next())
+  {
+    if (*evid1 != prevev) { // primary particle has id=1
+      counterh++;
+      prevev = *evid1;
+    }
+  }
+  prevev = -1;
+  while (nt2.Next())
+  {
+    if (*evid2 != prevev) {
+      counterw++;
+      prevev = *evid2;
+    }
+  }
+  std::cout << "counted gas hits: " << counterh << std::endl;
+  std::cout << "counted watch hits: " << counterw << std::endl;
+}
 
 //  use output TTree for summary
 void summary(TString fname) {
