023bf3c042781907d814370dd61029803e53b41b
Tom Goffrey <t.goffrey.1@warwick.ac.uk>
2021-05-05 14:50:23 +0100

    Minor format change to config.py

--- python/config.py
+++ python/config.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize'] = [4.5, 3]
 plt.rcParams['figure.dpi'] = 200
 plt.rcParams['lines.linewidth'] = 1.5
-plt.rcParams['lines.dashed_pattern'] = [3.5,3.5]
+plt.rcParams['lines.dashed_pattern'] = [3.5, 3.5]
