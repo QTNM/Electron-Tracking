c980bd95f65af427ce07d113c2c22ed8f1e49259
Tom Goffrey <t.goffrey.1@warwick.ac.uk>
2021-06-07 12:54:36 +0100

    Improved variable names in ford1991.py

--- python/ford1991.py
+++ python/ford1991.py
@@ -165,27 +165,27 @@ def analytic_solution_3d(time, b_field=np.array([0, 0, 1]),
     # Calculate vpara, vperp
     vpar, vperp = decompose_velocity(vel0, b_field)
     # Now rotate coordinate system so B aligned with z-axis
-    M = rotate_b_field(b_field)
+    mrot = rotate_b_field(b_field)
     # Velocity in rotated coordinate system
-    v = np.dot(M, vperp)
+    vrot = np.dot(mrot, vperp)
 
     # Calculate b-field magnitude for 1D solution
     bmag = np.linalg.norm(b_field)
 
     # Analytic 1D solution
-    x_b, y_b, vx_b, vy_b = analytic_solution(time, b_field=bmag, vel0=v[0:2],
+    x_b, y_b, vx_b, vy_b = analytic_solution(time, b_field=bmag, vel0=vrot[0:2],
                                              mass=mass, charge=charge, tau=tau)
 
     # Calculate full solution by transforming back and adding on vpara
-    N = rotate_b_field_inverse(b_field)
+    minv = rotate_b_field_inverse(b_field)
 
     # z_b = 0 for all time
-    x_soln = N[0, 0] * x_b + N[0, 1] * y_b + vpar[0] * time
-    y_soln = N[1, 0] * x_b + N[1, 1] * y_b + vpar[1] * time
-    z_soln = N[2, 0] * x_b + N[2, 1] * y_b + vpar[2] * time
-    vx_soln = N[0, 0] * vx_b + N[0, 1] * vy_b + vpar[0]
-    vy_soln = N[1, 0] * vx_b + N[1, 1] * vy_b + vpar[1]
-    vz_soln = N[2, 0] * vx_b + N[2, 1] * vy_b + vpar[2]
+    x_soln = minv[0, 0] * x_b + minv[0, 1] * y_b + vpar[0] * time
+    y_soln = minv[1, 0] * x_b + minv[1, 1] * y_b + vpar[1] * time
+    z_soln = minv[2, 0] * x_b + minv[2, 1] * y_b + vpar[2] * time
+    vx_soln = minv[0, 0] * vx_b + minv[0, 1] * vy_b + vpar[0]
+    vy_soln = minv[1, 0] * vx_b + minv[1, 1] * vy_b + vpar[1]
+    vz_soln = minv[2, 0] * vx_b + minv[2, 1] * vy_b + vpar[2]
 
     return x_soln, y_soln, z_soln, vx_soln, vy_soln, vz_soln
 
@@ -208,10 +208,10 @@ def rhs_3d(t, x, charge, mass, tau, omega0=np.array([0., 0., 1.]),
     if calc_b_field is None:
         omega = omega0
     else:
-        b = calc_b_field(x[0], x[1], x[2])
-        omega = calculate_omega(b, charge=charge, energy=0.0, mass=mass)
+        bfield = calc_b_field(x[0], x[1], x[2])
+        omega = calculate_omega(bfield, charge=charge, energy=0.0, mass=mass)
 
-    d = 1 / (1 + tau**2 * np.dot(omega, omega))
+    denom = (1 + tau**2 * np.dot(omega, omega))
     accx = omega[2] * x[4] - omega[1] * x[5] - tau * (omega[2]**2
                                                       + omega[1]**2) * x[3]
     accx += tau * omega[0] * (omega[2] * x[5] + omega[1] * x[4])
@@ -221,9 +221,9 @@ def rhs_3d(t, x, charge, mass, tau, omega0=np.array([0., 0., 1.]),
     accz = omega[1] * x[3] - omega[0] * x[4] - tau * (omega[0]**2
                                                       + omega[1]**2) * x[5]
     accz += tau * omega[2] * (omega[0] * x[3] + omega[1] * x[4])
-    accx *= d
-    accy *= d
-    accz *= d
+    accx /= denom
+    accy /= denom
+    accz /= denom
 
     # Power according to Larmor formula
     power = tau * mass * (accx**2 + accy**2 + accz**2)
@@ -255,7 +255,7 @@ def solve_3d(n_rotations, b0, v0, mass=me, charge=-qe, tau=0.0,
 
     # Check if magnetic field is scalar, if so, pass to 1D solve
     if np.size(b0) == 1:
-        return solve(n_rotations, b_field, vel0, mass, charge, tau)
+        return solve(n_rotations, b0, v0, mass, charge, tau)
 
     # Calculate omega vector to calculate initial timestep
     wvec = calculate_omega(b0, charge=charge, energy=0.0, mass=mass)
